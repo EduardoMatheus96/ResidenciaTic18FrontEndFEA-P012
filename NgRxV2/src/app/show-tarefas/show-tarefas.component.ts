79901a9d9d8c752485aa5ca0b7330631dd5d5d48
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-05-28 18:20:40 -0300

    Adicionado o Signals aos Components

--- NgRxV2/src/app/show-tarefas/show-tarefas.component.ts
+++ NgRxV2/src/app/show-tarefas/show-tarefas.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
+import { tarefasStore } from './../store/tarefa.store';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TarefaState } from '../store/tarefa.reducer';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { selectorSelecionaTarefa } from '../store/tarefa.seletors';
 import { Tarefa } from '../tarefa.model';
-import { removerTarefa } from '../store/tarefa.actions';
 
 @Component({
   selector: 'app-show-tarefas',
@@ -15,19 +13,9 @@ import { removerTarefa } from '../store/tarefa.actions';
   styleUrls: ['./show-tarefas.component.css']
 })
 export class ShowTarefasComponent {
-  tarefas: Tarefa[] = [{id: '1', descricao: 'Descrição 1'},];
-  tasks$!: Observable<TarefaState>;
-
-  constructor(private store:Store<{tarefas: TarefaState}>) { }
-
-  ngOnInit() {
-    this.tasks$ = this.store.select(selectorSelecionaTarefa);
-    this.tasks$.subscribe((t) => {  
-      this.tarefas = t.tarefas;
-    });
-  }
+  tarefasStore = inject(tarefasStore);
 
   removeTarefa(id: string) {
-    this.store.dispatch(removerTarefa({id: id}));
+    this.tarefasStore.removeTarefa(id);
   }
 }
